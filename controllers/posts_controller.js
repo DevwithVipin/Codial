edf0e23130012af231656c7eb439ada150edb6e0
codermonkvipin <codermonkvipin@gmail.com>
2023-09-23 01:35:06 +0530

    added post with ajax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,6 +7,15 @@ module.exports.create = async function (req, res) {
             content: req.body.content,
             user: req.user._id
         });
+        if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
+
         req.flash('success', 'Post published!');
         return res.redirect('back');
     } catch (err) {
