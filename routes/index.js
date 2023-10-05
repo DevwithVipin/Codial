085c912c5a24c865250addfb7ce669e79d1ff613
codermonkvipin <codermonkvipin@gmail.com>
2023-10-05 23:39:49 +0530

    added jwt authentication

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,17 @@ const homeController = require('../controllers/home_controller');
 
 console.log('router loaded');
 
+
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
 
-// For any further routes, access from here
-// router.use('/routerName', require('./routerfile'));
+
+router.use('/api', require('./api'));
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
 
 module.exports = router;
